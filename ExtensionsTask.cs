2799e6f1b14baf9ef383f02d63a9c52f0e2f0406
nikitagordeev10 <nikitagordeev10@yandex.ru>
2024-10-24 23:59:14 +0300

    оформил для github

---
+++ ExtensionsTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_slideviews;
+
+public static class ExtensionsTask {
+    public static double Median(this IEnumerable<double> items) { // метод для нахождения медианы последовательности чисел
+
+        var sorted = items.OrderBy(x => x).ToList(); // сортировать последовательность по возрастанию
+        var elementСount = sorted.Count; // длина последовательности
+
+        if (elementСount == 0) { // последовательность пуста
+            throw new InvalidOperationException("Последовательность не содержит элементов"); // исключение
+        }
+
+        var mid = elementСount / 2; // среднее значение
+        var median = elementСount % 2 != 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / 2; // серединный элемент списка
+        return median;
+    }
+
+    public static IEnumerable<(T First, T Second)> Bigrams<T>(this IEnumerable<T> items) { // получения пар элементов из последовательности
+        var prev = default(T); // значение по умолчанию для типа T
+        var first = true;
+
+        foreach (var curr in items) {
+            if (!first) { // не первый элемент
+                yield return (prev, curr); // сформировать пару из предыдущего и текущего элементов
+            }
+            prev = curr; // использовать в следующей паре
+            first = false; // не возвращать пары с первым элементом
+        }
+    }
+}
---
+++ ParsingTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace linq_slideviews {
+    public class ParsingTask {
+        public static IDictionary<int, SlideRecord> ParseSlideRecords(IEnumerable<string> lines) {
+            var slideTypeMapping = new Dictionary<string, SlideType>()
+            {
+                {"theory", SlideType.Theory},
+                {"quiz", SlideType.Quiz},
+                {"exercise", SlideType.Exercise}
+            };
+
+            return lines.Skip(1)
+                .Select(line => {
+                    var lineParams = line.Split(new[] { ';' }, 3, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineParams.Length < 3 || !int.TryParse(lineParams[0], out int slideID))
+                        return null;
+
+                    if (!slideTypeMapping.TryGetValue(lineParams[1], out SlideType slideType))
+                        return null;
+
+                    return new SlideRecord(slideID, slideType, lineParams[2]);
+                })
+                .Where(slideRecord => slideRecord != null)
+                .ToDictionary(slideRecord => slideRecord.SlideId);
+        }
+
+        public static IEnumerable<VisitRecord> ParseVisitRecords(IEnumerable<string> lines, IDictionary<int, SlideRecord> slides) {
+            return lines.Skip(1)
+                .Select(line => {
+                    var lineParams = line.Split(new[] { ';' }, 3, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineParams.Length < 3
+                        || !int.TryParse(lineParams[0], out int userID)
+                        || !int.TryParse(lineParams[1], out int slideID)
+                        || !DateTime.TryParseExact(lineParams[2], "yyyy-MM-dd;HH:mm:ss",
+                            CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime)) {
+                        throw new FormatException($"Wrong line [{line}]");
+                    }
+
+                    if (!slides.TryGetValue(slideID, out SlideRecord slideRecord)) {
+                        throw new FormatException($"Slide with ID {slideID} not found in line [{line}]");
+                    }
+
+                    return new VisitRecord(userID, slideID, dateTime, slideRecord.SlideType);
+                });
+        }
+    }
+}
